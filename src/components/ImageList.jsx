202b3ba4081a95c68bc913277917adf9b918e9c3
Patrick Duvall <duvallp208@gmail.com>
2021-01-10 15:24:19 -0700

    add alt and key

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const ImageList = (props) => {
-  const images = props.images.map(image => {
-    return <img src = {image.urls.regular} />
+  const images = props.images.map(({alt_description, id, urls}) => {
+    return <img key={id} alt={alt_description} src = {urls.regular} />
   })
   return <div>{images}</div>
 }
